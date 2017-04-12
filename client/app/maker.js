c5d0a2fa6edcbf441cb4df45b19e29787bdc6cca
AJKaufman <ak3012@rit.edu>
2017-04-12 15:16:54 -0400

    updated spelling mistakes

--- client/app/maker.js
+++ client/app/maker.js
@@ -40,6 +40,83 @@ const renderDomo = function() {
 };
 
 
+const renderDomoList = function() {
+  if(this.state.data.length === 0) {
+    return (
+      <div className="domoList">
+        <h3 className="emptyDomo">No Domos yet</h3>
+      </div>
+    );
+  }
+  
+  const domoNodes = this.state.data.map(function(domo) {
+    return (
+    <div key={domo._id} className="domo">
+      <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
+      <h3 className="domoName"> Name: {domo.name} </h3>
+      <h3 className="domoAge"> Age: {domo.age} </h3>
+    </div>
+    );    
+  });
+  
+  return (
+    <div className="domoList">
+      {domoNodes}
+    </div>
+  );
+};
+
+const setup = function(csrf) {
+  DomoFormClass = React.createClass({
+    handleSubmit: handleDomo,
+    render: renderDomo,
+  });
+    
+  DomoListClass = React.createClass({
+    loadDomosFromServer: function() {
+      sendAjax('GET', '/getDomos', null, function(data) {
+        this.setState({data:data.domos});
+      }.bind(this));
+    },
+    getInitialState: function() {
+      return {data: []};
+    },
+    componentDidMount: function() {
+      this.loadDomosFromServer();
+    },
+    render: renderDomoList
+  });
+  
+  domoForm = ReactDOM.render(
+    <DomoFormClass csrf={csrf} />, document.querySelector("#makeDomo")
+  );
+  
+  domoRenderer = ReactDOM.render(
+    <DomoListClass />, document.querySelector("#domos");
+  );
+};
+
+const getToken = () => {
+  sendAjax('GET', '/getToken', null, (result) => {
+    setup(result.csrfToken);
+  });
+};
+
+$(document).ready(function() {
+  getToken();
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- hosted/bundle.js
+++ hosted/bundle.js
@@ -1,55 +1,5 @@
 "use strict";
 
-var domoRenderer = void 0;
-var domoForm = void 0;
-var DomoFormClass = void 0;
-var DomoListClass = void 0;
-
-var handleDomo = function handleDomo(e) {
-  e.preventDefault();
-
-  $("#domoMessage").animate({ width: 'hide' }, 350);
-
-  if ($("#domoName").val() == '' || $("#domoAge").val() == '') {
-    handleError("RAWR! All fields are required");
-    return false;
-  }
-
-  sendAjax('POST', $("#domoForm").attr("action"), $("#domoForm").serialize(), function () {
-    domoRenderer.loadDomosFromServer();
-  });
-
-  return false;
-};
-
-var renderDomo = function renderDomo() {
-  return React.createElement(
-    "form",
-    { id: "domoForm",
-      onSubmit: this.handleSubmit,
-      name: "domoForm",
-      action: "/maker",
-      method: "POST",
-      className: "domoForm"
-    },
-    React.createElement(
-      "label",
-      { htmlFor: "name" },
-      "Name: "
-    ),
-    React.createElement("input", { id: "domoName", type: "text", name: "name", placeholder: "Domo Name" }),
-    React.createElement(
-      "label",
-      { htmlFor: "age" },
-      "Age: "
-    ),
-    React.createElement("input", { id: "domoAge", type: "text", name: "age", placeholder: "Domo Age" }),
-    React.createElement("input", { type: "hidden", name: "_csrf", value: this.props.csrf }),
-    React.createElement("input", { className: "makeDomoSubmit", type: "submit", value: "Make Domo" })
-  );
-};
-"use strict";
-
 var handleError = function handleError(message) {
   $("#errorMessage").text(message);
   $("#domoMessage").animate({ width: 'toggle' }, 350);
